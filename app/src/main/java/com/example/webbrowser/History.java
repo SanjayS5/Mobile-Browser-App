ed7bea477d1df57ef855a3dc268b2a8e82af3efa
Sanjay Sudhakaran <sanjaysudhakaran@gmail.com>
2019-10-04 15:23:47 +1300

    Historical urls are passed to the History activity

--- app/src/main/java/com/example/webbrowser/History.java
+++ app/src/main/java/com/example/webbrowser/History.java
@@ -2,7 +2,14 @@ package com.example.webbrowser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebBackForwardList;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class History extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+//        Intent intent = getIntent();
+//        Serializable wbfl = intent.getSerializableExtra(MainActivity.EXTRA_WBFL);
+
+        Intent intent = this.getIntent();
+
+        ArrayList<String> urlList = intent.getStringArrayListExtra(MainActivity.EXTRA_URL);
+
+        for (String a : urlList) {
+            Log.d("sanjay", "URL is " + a);
+        }
+
+
+
+
+//        List<WebBackForwardList> wbflObjects = new ArrayList<WebBackForwardList>();
+//        for (String key : bundle.keySet()) {
+//            Log.d("sanjay", (String) bundle.getSerializable(key));
+//            wbflObjects.add((WebBackForwardList)bundle.getSerializable(key));
+//        }
+
+
     }
 }
+
+
--- app/src/main/java/com/example/webbrowser/MainActivity.java
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.webbrowser;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -24,12 +19,14 @@ import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_URL = "com.example.webbrowser.EXTRA_URL";
     // TODO: 2019-10-03 Create progress bar for web loading
 
     @Override
@@ -154,21 +151,38 @@ public class MainActivity extends AppCompatActivity {
         } else if (id == R.id.historyMenuOption) {
             Log.d("sanjay", "History menu activated");
             WebView webView = this.findViewById(R.id.webview);
-            getBackForwardList(webView);
+            ArrayList<String> urlList = getBackForwardList(webView);
+
+//            Intent intent = new Intent(this, History);
+//            intent.putExtra()
+
+            openHistory(urlList);
 
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void getBackForwardList(WebView webView){
+    public ArrayList<String> getBackForwardList(WebView webView){
         WebBackForwardList currentList = webView.copyBackForwardList();
+
+        ArrayList<String> urls = new ArrayList<String>();
+
         int currentSize = currentList.getSize();
-        for(int i = 0; i < currentSize; ++i)
-        {
+        for(int i = 0; i < currentSize; ++i) {
             WebHistoryItem item = currentList.getItemAtIndex(i);
             String url = item.getUrl();
             Log.d("sanjay", "The URL at index: " + Integer.toString(i) + " is " + url );
+            urls.add(url);
         }
+
+        return urls;
+    }
+
+    public void openHistory(ArrayList<String> urlList) {
+        Intent intent = new Intent(this, History.class);
+        intent.putStringArrayListExtra(EXTRA_URL, urlList);
+//        intent.putExtras(EXTRA_URL, urlList);
+        startActivity(intent);
     }
 }
