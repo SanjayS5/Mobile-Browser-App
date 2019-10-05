68cfc5271d73cfdff9e96cb671049df8c778da6d
Sanjay Sudhakaran <sanjaysudhakaran@gmail.com>
2019-10-06 11:57:58 +1300

    Settings switch preference saves, but not working

--- app/src/main/java/com/example/webbrowser/SettingsActivity.java
+++ app/src/main/java/com/example/webbrowser/SettingsActivity.java
@@ -1,10 +1,15 @@
 package com.example.webbrowser;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import java.util.prefs.PreferenceChangeListener;
 
 
 public class SettingsActivity extends PreferenceActivity {
@@ -23,6 +28,27 @@ public class SettingsActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            bindBooleanValue(findPreference("switch_preference_1"));
         }
     }
+
+    private static void bindBooleanValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(listener);
+        listener.onPreferenceChange(preference,
+                PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getBoolean(preference.getKey(), true));
+
+    }
+
+    private static Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            Log.d("sanjay", "newValue is " + newValue);
+            preference.setDefaultValue(newValue);
+            return true;
+        }
+    };
+
+
 }
